99819b50974c036af3d842323ee645f2d37c69c0
vellulellu <li94ve@gmail.com>
2017-04-04 22:35:24 +0300

    Got rid of delays and added potentiometer
    Trying this tomorrow 5.4

--- matrice_100_camera_trig.ino
+++ matrice_100_camera_trig.ino
@@ -5,24 +5,34 @@ Servo myservo;  // create servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
 
+int inVal = analogRead(A1);
+int outVal = map(inVal, 0, 1023, 400, 5000);  //set trig. freg. between 2 last values
+long previousMillis = 0; 
+long interval = 10000; //wait this time before starting to photograph
 void setup() {
   myservo.attach(3);  // attaches the servo on pin 3 to the servo object
+  pinMode(3, OUTPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop() {
+  
+unsigned long currentMillis = millis();
 
-
+  
 if(digitalRead(4) ==LOW)
 {
+  
   digitalWrite(13, HIGH);
   myservo.write(90);
-  delay(60000);   //wait before starting to photograph
-  
-while(digitalRead(4) == LOW)
+
+if(currentMillis - previousMillis > interval) {
+    //Save the last time someone used interval 
+    previousMillis = currentMillis;   
+
+  while(digitalRead(4) == LOW)
 
 {
-
-  
   for (pos = 150; pos <= 0; pos += 4) { // first "pos" determines start and second end position. last pos is speed of movement 
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(30);                       // waits 15ms for the servo to reach the position
@@ -31,14 +41,11 @@ while(digitalRead(4) == LOW)
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(30);                       // waits 15ms for the servo to reach the position
   }
-  delay(2500);              //CHANGE THIS NUMBER TO SET PHOTO FREQUENCY IN ms
+  delay(outVal);              //CHANGE THIS NUMBER TO SET PHOTO FREQUENCY IN ms
 }
-
 }
-else
-{
+}
   digitalWrite(13, LOW);
 myservo.write(0);
-}
 }
 
